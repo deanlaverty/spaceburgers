66e17a63f9113fc4889770567a4e97166b78629e
Dean Laverty <deannooel@gmail.com>
2022-07-15 11:21:15 +0100

    Added caching to buns and fillings endpoints

--- app/Http/Controllers/GetBunsController.php
+++ app/Http/Controllers/GetBunsController.php
@@ -7,13 +7,19 @@ namespace App\Http\Controllers;
 use App\Models\Bun;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class GetBunsController
 {
     public function __invoke(Request $request): JsonResponse
     {
+        $buns = Cache::remember('buns', 60 * 60, function () {
+            return Bun::all();
+        });
+
         return new JsonResponse([
-            'data' => Bun::all()
+            'data' => $buns
         ], 200);
     }
 }
--- app/Http/Controllers/GetFillingsController.php
+++ app/Http/Controllers/GetFillingsController.php
@@ -7,13 +7,18 @@ namespace App\Http\Controllers;
 use App\Models\Filling;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class GetFillingsController
 {
     public function __invoke(Request $request): JsonResponse
     {
+        $fillings = Cache::remember('fillings', 60 * 60, function () {
+            return Filling::all();
+        });
+
         return new JsonResponse([
-            'data' => Filling::all()
+            'data' => $fillings
         ], 200);
     }
 }
